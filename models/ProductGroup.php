612728f9273ca5c9a09c78748c9c92c6f4fbe5ae
Wes Gilleland <wes.gilleland@gmail.com>
2020-01-24 19:45:05 -0500

    Mapped ProductGroup

---
+++ models/ProductGroup.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+
+namespace Grocy\Models;
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Grocy\Models\SuperClasses\NameDescriptionEntity;
+
+/**
+ * Class ProductGroup
+ * @package Grocy\Models
+ * @Entity
+ * @Table(name="product_groups")
+ */
+class ProductGroup extends NameDescriptionEntity
+{
+}
